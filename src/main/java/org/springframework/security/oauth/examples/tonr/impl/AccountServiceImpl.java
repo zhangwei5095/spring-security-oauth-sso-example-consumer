c79ebc20d1a4be4b99bf80f443ec55f0268a8d34
jimmidyson <jimmi@feedthecoffers.org.uk>
2012-05-02 17:04:48 +0100

    Fix to get details from OAuth server

--- src/main/java/org/springframework/security/oauth/examples/tonr/impl/AccountServiceImpl.java
+++ src/main/java/org/springframework/security/oauth/examples/tonr/impl/AccountServiceImpl.java
@@ -1,33 +1,26 @@
 package org.springframework.security.oauth.examples.tonr.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth.examples.tonr.AccountService;
 import org.springframework.security.oauth.examples.tonr.UserInfo;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.web.client.RestOperations;
 
 public class AccountServiceImpl implements AccountService {
-  
-  private final Logger log = LoggerFactory.getLogger(getClass());
 
 	private String accountDetailsURL;
+	
 	private RestOperations oauthRestTemplate;
-
-	public UserInfo getCurrentUserInfo() {
-	  UserInfo userInfo = new UserInfo();
-	  userInfo.setUserId("testuser");
-	  log.info("returning user");
-	  return userInfo;
-	  //return oauthRestTemplate.getForObject(accountDetailsURL, UserInfo.class);
-	}
-
+	
 	public void setAccountDetailsURL(String accountDetailsURL) {
-		this.accountDetailsURL = accountDetailsURL;
+	  this.accountDetailsURL = accountDetailsURL;
 	}
 	
 	public void setOAuthRestTemplate(OAuth2RestTemplate oauthRestTemplate) {
-		this.oauthRestTemplate = oauthRestTemplate;
+	  this.oauthRestTemplate = oauthRestTemplate;
+	}
+
+	public UserInfo getCurrentUserInfo() {
+	  return oauthRestTemplate.getForObject(accountDetailsURL, UserInfo.class);
 	}
 
 }
--- src/main/java/org/springframework/security/oauth/examples/tonr/mvc/AccountController.java
+++ src/main/java/org/springframework/security/oauth/examples/tonr/mvc/AccountController.java
@@ -1,31 +1,27 @@
 package org.springframework.security.oauth.examples.tonr.mvc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth.examples.tonr.AccountService;
 import org.springframework.security.oauth.examples.tonr.UserInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AccountController {
   
-  private final Logger log = LoggerFactory.getLogger(getClass());
-  
   private AccountService accountService;
 
-  @RequestMapping(value = "/account")
+  public void setAccountService(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  @RequestMapping(value = "/account", method = RequestMethod.GET)
   public ModelAndView accountDetails() {
     UserInfo userInfo = accountService.getCurrentUserInfo();
     ModelAndView template = new ModelAndView("layout:account");
     template.addObject("userInfo", userInfo);
-    log.info("Added user {}", userInfo.getUserId());
     return template;
-  }
-
-  public void setAccountService(AccountService accountService) {
-    this.accountService = accountService;
   }
 
 }
